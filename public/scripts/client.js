a69470faf0ddb9ddea0d0a97e762c59bb7b133c6
Ross <ross@tests-MacBook-Pro.local>
2018-01-19 15:05:01 -0600

    finished base mode

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,13 +1,30 @@
 const giphyApp = angular.module('giphyApp',[]);
 
-giphyApp.controller('SearchController', function(){
+giphyApp.controller('SearchController', ['$http', function($http){
     const self = this;
-    console.log('Hi');
-    
-});
 
-giphyApp.controller('RandomController', function(){
+    self.searchGif = function(text){
+        const config = {
+            params: {api_key: 'f8QlTzrkob8Xbdyb9z5tyoAda2c0gQ5G', q: text}
+        }
+        $http.get('http://api.giphy.com/v1/gifs/search', config)
+        .then(function(response){
+            self.searchedGif = response.data.data;
+            console.log(response.data.data);
+    })}
+}]);
+
+giphyApp.controller('RandomController', ['$http', function($http){
     const self = this;
-    console.log('There');
-    
-});
+
+    const config = {
+        params: {api_key: 'f8QlTzrkob8Xbdyb9z5tyoAda2c0gQ5G'}
+    }
+self.randomizeGif = function(){
+    $http.get('http://api.giphy.com/v1/gifs/random', config)
+        .then(function(response){
+            self.randomGif = response.data.data;
+            console.log(response.data.data);
+        })}
+     
+}]);
